1ed8f34d9bfd59be6936fda847cd9d89417fe7ce
daryl-sen <tsdaryl@gmail.com>
2020-10-08 13:04:29 -0700

    Applied new project model method to Projects model

--- test.py
+++ test.py
@@ -3,18 +3,6 @@ from application.models import *
 
 import datetime as dt
 
-# cu = Users.query.get(1)
+cu = Users.query.get(1)
 
-# projects = cu.collab_projects.filter_by(id=2).all()
-
-
-# tp = Projects.query.get(2)
-# tp.bugs
-
-
-# print(tp.bugs.filter_by(id=3).all())
-
-
-p = Projects.query.get(1)
-
-p.get_counters(dt.datetime.now())
+print(cu.collab_projects.order_by(Projects.last_activity).all())
