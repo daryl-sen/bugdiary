3993bf4a38a8fecf7ab61dbc618041bac26ad777
daryl <tsdaryl@gmail.com>
2021-05-15 16:44:30 -0700

    make responsive adjustments to TwoColumnLayout

--- src/components/TwoColumnLayout.js
+++ src/components/TwoColumnLayout.js
@@ -3,11 +3,16 @@ import "./TwoColumnLayout.scss";
 export default function TwoColumnLayout(props) {
   return (
     <div className="two-column-layout">
-      <main>
-        <div className="bg-white-block"></div>
-      </main>
+      <main>{props.children || "No content"}</main>
       <aside>
-        <div className="lined-block"></div>
+        <div className="lined-block">
+          <h3>BugDiary</h3>
+          <p>Create your own bug diary today! Get started here.</p>
+          <button>Sign Up</button>
+        </div>
+        <div className="lined-block">
+          <h3>Related Issues</h3>
+        </div>
       </aside>
     </div>
   );
--- src/stories/TwoColumnLayout.stories.js
+++ src/stories/TwoColumnLayout.stories.js
@@ -3,4 +3,6 @@ import TwoColumnLayout from "../components/TwoColumnLayout";
 
 export default { title: "Two column layout" };
 
-export const FilledTwoColumnLayout = () => <TwoColumnLayout />;
+export const FilledTwoColumnLayout = () => (
+  <TwoColumnLayout>Hello</TwoColumnLayout>
+);
