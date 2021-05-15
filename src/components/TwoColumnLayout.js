60a47819dbc4368be60040c9c018b7839fdf85f2
daryl <tsdaryl@gmail.com>
2021-05-15 16:27:44 -0700

    create TwoColumnLayout and its story

---
+++ src/components/TwoColumnLayout.js
@@ -0,0 +1,12 @@
+import "./TwoColumnLayout.scss";
+
+export default function TwoColumnLayout(props) {
+  return (
+    <div className="two-column-layout">
+      <main>
+        <h1>Hello</h1>
+      </main>
+      <aside>Hi</aside>
+    </div>
+  );
+}
---
+++ src/stories/TwoColumnLayout.stories.js
@@ -0,0 +1,6 @@
+import { React } from "react";
+import TwoColumnLayout from "../components/TwoColumnLayout";
+
+export default { title: "Two column layout" };
+
+export const FilledTwoColumnLayout = () => <TwoColumnLayout />;
