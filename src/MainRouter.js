ea4f31705657a25fbeeed03f6b365b80fbef04c4
daryl <tsdaryl@gmail.com>
2021-05-15 19:34:46 -0700

    pass toggleMenu to TopBar from MainRouter

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import "./MainRouter.scss";
 import TopBar from "./components/layout/TopBar";
 import SideNav from "./components/layout/SideNav";
 
 export default function MainRouter(props) {
+  const [menuToggle, setMenuToggle] = useState(false);
+
+  const toggleMenu = () => {
+    menuToggle ? setMenuToggle(false) : setMenuToggle(true);
+  };
+
   return (
     <>
-      <TopBar />
+      <TopBar toggleMenu={toggleMenu} />
       <SideNav />
       <main>
         <h1>Hello</h1>
--- src/components/layout/TopBar.js
+++ src/components/layout/TopBar.js
@@ -5,7 +5,7 @@ export default function TopBar(props) {
   return (
     <header id="top-bar">
       <button id="side-nav-toggle">
-        <CgMenu />
+        <CgMenu onClick={props.toggleMenu} />
       </button>
       <div id="title">BugDiary.com</div>
     </header>
