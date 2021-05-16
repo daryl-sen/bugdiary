e3e9d3e6a676d123bb7dd4b7d7e88ecb0eec6aca
daryl <tsdaryl@gmail.com>
2021-05-15 19:48:43 -0700

    replace menu icon with cross in TopBar when SideNav is open

--- src/components/layout/TopBar.js
+++ src/components/layout/TopBar.js
@@ -1,11 +1,15 @@
 import "./TopBar.scss";
-import { CgMenu } from "react-icons/cg";
+import { CgMenu, CgClose } from "react-icons/cg";
 
 export default function TopBar(props) {
   return (
     <header id="top-bar">
       <button id="side-nav-toggle">
-        <CgMenu onClick={props.toggleMenu} />
+        {props.menuState ? (
+          <CgClose onClick={props.toggleMenu} />
+        ) : (
+          <CgMenu onClick={props.toggleMenu} />
+        )}
       </button>
       <div id="title">BugDiary.com</div>
     </header>
