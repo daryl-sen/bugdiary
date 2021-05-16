cd921b56c6470a9adbe4128c1bcf076e2f0f43f2
daryl <tsdaryl@gmail.com>
2021-05-15 19:45:11 -0700

    make SideNav out of view by default

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -4,16 +4,16 @@ import TopBar from "./components/layout/TopBar";
 import SideNav from "./components/layout/SideNav";
 
 export default function MainRouter(props) {
-  const [menuToggle, setMenuToggle] = useState(false);
+  const [menuState, setMenuToggle] = useState(false);
 
   const toggleMenu = () => {
-    menuToggle ? setMenuToggle(false) : setMenuToggle(true);
+    menuState ? setMenuToggle(false) : setMenuToggle(true);
   };
 
   return (
     <>
-      <TopBar toggleMenu={toggleMenu} />
-      <SideNav />
+      <TopBar toggleMenu={toggleMenu} menuState={menuState} />
+      <SideNav menuState={menuState} />
       <main>
         <h1>Hello</h1>
       </main>
--- src/components/layout/SideNav.js
+++ src/components/layout/SideNav.js
@@ -1,8 +1,16 @@
 import "./SideNav.scss";
 
 export default function SideNav(props) {
+  const transformInfo = {};
+
+  if (props.menuState) {
+    transformInfo.transform = "translate(0px)";
+  } else {
+    transformInfo.transform = "translate(-300px)";
+  }
+
   return (
-    <nav>
+    <nav style={transformInfo}>
       <div>Hello</div>
       <div>footer</div>
     </nav>
