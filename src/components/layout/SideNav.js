639a114b4cbfabe3064417154524a2c9f515d343
daryl <tsdaryl@gmail.com>
2021-05-15 19:14:45 -0700

    create SideNav and corresponding story

---
+++ src/components/layout/SideNav.js
@@ -0,0 +1,10 @@
+import "./SideNav.scss";
+
+export default function SideNav(props) {
+  return (
+    <nav>
+      <div>Hello</div>
+      <div>footer</div>
+    </nav>
+  );
+}
---
+++ src/stories/SideNav.stories.js
@@ -0,0 +1,6 @@
+import { React } from "react";
+import SideNav from "../components/layout/SideNav";
+
+export default { title: "Sidebar Navigation" };
+
+export const FilledTwoColumnLayout = () => <SideNav>Hello</SideNav>;
