2a0bc8d9a024b122b0efba7f18ab4c10d901cd45
daryl <tsdaryl@gmail.com>
2021-05-15 19:20:44 -0700

    fill in MainRouter

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,10 +1,12 @@
 import "./MainRouter.scss";
 import TopBar from "./components/layout/TopBar";
+import SideNav from "./components/layout/SideNav";
 
 export default function MainRouter(props) {
   return (
     <>
       <TopBar />
+      <SideNav />
       <main>
         <h1>Hello</h1>
       </main>
