d13c0bf33dce58cdb07920bc8057ac652ac6e498
Daryl <tsdaryl@gmail.com>
2021-05-19 15:49:14 -0700

    create app.js express app

---
+++ app.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+const { sequelize, User } = require("./models");
+const app = express();
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  return res.send("Hello");
+});
+
+// app.post("/create-user", async (req, res) => {
+//   const { name, email, role } = req.body;
+
+//   try {
+//     const user = await User.create({ name, email, role });
+//     return res.json(user);
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(500).json(error);
+//   }
+// });
+
+app.listen(PORT, async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connected to database");
+  } catch (error) {
+    return console.log(error);
+  }
+  // await sequelize.sync({ force: true });
+  console.log(`Running on port: ${PORT}`);
+});
