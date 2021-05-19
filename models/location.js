854afbb197fabc18e698a0d0022f7988fe882cfb
Daryl <tsdaryl@gmail.com>
2021-05-19 16:53:05 -0700

    update associations for Location model

--- models/location.js
+++ models/location.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Location extends Model {
     /**
@@ -9,15 +7,19 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({ Diary, Issue }) {
+      this.belongsTo(Diary, { foreignKey: "diary_id" });
+      this.hasMany(Issue, { foreignKey: "location_id" });
     }
-  };
-  Location.init({
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Location',
-  });
+  }
+  Location.init(
+    {
+      name: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Location",
+    }
+  );
   return Location;
 };
