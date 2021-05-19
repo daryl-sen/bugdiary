b12d16660f80701c65c6ff0fbbe5bf9d88a52b0e
Daryl <tsdaryl@gmail.com>
2021-05-19 16:50:12 -0700

    update associations for Diary model

--- models/diary.js
+++ models/diary.js
@@ -7,8 +7,13 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ User, Issue, Version, Type, IssueTag, Tag, Location }) {
-      this.belongsTo(User);
+    static associate({ User, Issue, Version, Type, Tag, Location }) {
+      this.belongsTo(User, { foreignKey: "user_id" });
+      this.hasMany(Issue, { foreignKey: "diary_id" });
+      this.hasMany(Version, { foreignKey: "diary_id" });
+      this.hasMany(Type, { foreignKey: "diary_id" });
+      this.hasMany(Tag, { foreignKey: "diary_id" });
+      this.hasMany(Location, { foreignKey: "diary_id" });
     }
   }
   Diary.init(
