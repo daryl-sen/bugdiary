cecb41b0c71269115716206d6862904c8718b954
Daryl <tsdaryl@gmail.com>
2021-05-19 18:42:07 -0700

    add many-to-many association between Issue and Tag

--- models/tag.js
+++ models/tag.js
@@ -7,8 +7,9 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ Diary }) {
+    static associate({ Diary, Issue }) {
       this.belongsTo(Diary, { foreignKey: "diary_id" });
+      this.belongsToMany(Issue, { through: "issue_tags", as: "tag_id" });
     }
   }
   Tag.init(
