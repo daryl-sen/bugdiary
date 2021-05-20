3796df961c53893813898678d2827a75e2804457
Daryl <tsdaryl@gmail.com>
2021-05-19 17:11:17 -0700

    add associations for User model

--- models/user.js
+++ models/user.js
@@ -7,8 +7,11 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({ Comment, Upvote, Diary, UserType }) {
+      this.belongsTo(UserType, { foreignKey: "user_type_id" });
+      this.hasMany(Comment, { foreignKey: "user_id" });
+      this.hasMany(Upvote, { foreignKey: "user_id" });
+      this.hasMany(Diary, { foreignKey: "user_id" });
     }
   }
   User.init(
