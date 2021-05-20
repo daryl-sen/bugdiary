ae663f8e06cace2538b6468b46e957419ca4f178
Daryl <tsdaryl@gmail.com>
2021-05-19 19:33:37 -0700

    add endpoint for creating user types

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,28 @@ const router = express.Router();
 
 module.exports = (models) => {
   const { User, UserType, Comment, Upvote } = models;
+
   router.get("/", (req, res) => {
     res.end("users route");
+  });
+
+  router.post("/create-role", async (req, res) => {
+    try {
+      const newUserType = await UserType.create({ ...req.body });
+      return res.json(JSON.stringify(newUserType));
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  router.post("/create", async (req, res) => {
+    try {
+      const newUser = await User.create({ ...req.body });
+      return res.json(JSON.stringify(newUser));
+    } catch (error) {
+      console.log(error);
+    }
+    res.end("An error has occured");
   });
 
   return router;
