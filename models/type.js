2b0a3b4acabcbfaea51fc63103b119ea9330ad88
Daryl <tsdaryl@gmail.com>
2021-05-19 17:11:54 -0700

    fix Type model

--- models/type.js
+++ models/type.js
@@ -7,7 +7,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({ Issue, Diary }) {
       this.hasMany(Issue, { foreignKey: "type_id" });
       this.belongsTo(Diary, { foreignKey: "diary_id" });
     }
