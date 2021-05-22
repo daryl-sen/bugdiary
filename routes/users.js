822ba236253c9ea4645924ae2c23bbb0d2d9268f
Daryl <tsdaryl@gmail.com>
2021-05-22 15:13:00 -0700

    add jwt generation to POST /api/users/login

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 
 module.exports = (models) => {
   const { User, UserType, Comment, Upvote } = models;
@@ -16,10 +19,28 @@ module.exports = (models) => {
 
   // login user
   router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
     try {
-      //
+      const targetUser = await User.findOne({
+        where: {
+          email,
+        },
+      });
+      if (!targetUser) {
+        return res.json({ error: "User does not exist." });
+      }
+      const accessToken = jwt.sign(
+        {
+          name: targetUser.display_name,
+          uuid: targetUser.uuid,
+        },
+        process.env.ACCESS_TOKEN_SECRET
+      );
+      res.json({ accessToken });
     } catch (error) {
-      //
+      console.log(error.errors);
+      res.json({ error: error.errors });
     }
   });
 
