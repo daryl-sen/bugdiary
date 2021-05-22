bb121a6adb207ff93f791db0a08f61d81adee453
Daryl <tsdaryl@gmail.com>
2021-05-21 21:53:35 -0700

    create endpoint skeletons for /api/users

--- routes/users.js
+++ routes/users.js
@@ -4,37 +4,49 @@ const router = express.Router();
 module.exports = (models) => {
   const { User, UserType, Comment, Upvote } = models;
 
-  router.post("/user-role", async (req, res) => {
+  // create user
+  router.post("/user", async (req, res) => {
     try {
-      const newUserType = await UserType.create({ ...req.body });
-      return res.json(JSON.stringify(newUserType));
+      //
     } catch (error) {
-      console.log(error);
+      //
     }
   });
 
-  router.delete("/user-role", async (req, res) => {
-    const { roleId } = req.body;
+  // login user
+  router.post("/login", async (req, res) => {
     try {
-      const targetRole = await UserType.findOne({
-        where: { id: roleId },
-      });
-      targetRole.destroy();
-      return res.end("Deleted");
+      //
     } catch (error) {
-      console.log(error);
+      //
     }
-    res.end("Error");
   });
 
-  router.post("/create", async (req, res) => {
+  // logout user
+  router.post("/logout", async (req, res) => {
     try {
-      const newUser = await User.create({ ...req.body });
-      return res.json(JSON.stringify(newUser));
+      //
     } catch (error) {
-      console.log(error);
+      //
     }
-    res.end("An error has occured");
+  });
+
+  // update user
+  router.patch("/user", async (req, res) => {
+    try {
+      //
+    } catch (error) {
+      //
+    }
+  });
+
+  // delete user
+  router.delete("/user", async (req, res) => {
+    try {
+      //
+    } catch (error) {
+      //
+    }
   });
 
   return router;
