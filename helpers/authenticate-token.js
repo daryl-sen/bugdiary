5de52e8d2075c9103702d4407af075ea0e861304
Daryl <tsdaryl@gmail.com>
2021-05-22 20:17:58 -0700

    move autheticateToken function into its own file at /helpers/authenticate-token.js

---
+++ helpers/authenticate-token.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ error: "JWT not received." });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, user) => {
+    if (error) {
+      return res.status(403).json({ error: "Invalid JWT" });
+    }
+  });
+  next();
+};
+
+module.exports = {
+  authenticateToken,
+};
--- routes/users.js
+++ routes/users.js
@@ -2,21 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const router = express.Router();
-const jwt = require("jsonwebtoken");
-
-const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-  if (!token) {
-    return res.status(401).json({ error: "JWT not received." });
-  }
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, user) => {
-    if (error) {
-      return res.status(403).json({ error: "Invalid JWT" });
-    }
-  });
-  next();
-};
+const { authenticateToken } = require("../helpers/authenticate-token");
 
 module.exports = (models) => {
   const { User, UserType, Comment, Upvote } = models;
