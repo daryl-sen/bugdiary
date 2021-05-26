642214c72bf2e069c88ede97ac00da004f1f7801
Daryl <tsdaryl@gmail.com>
2021-05-26 12:57:03 -0700

    create SignupForm stories

--- src/stories/formik-test.stories.js
+++
@@ -1,6 +0,0 @@
-import { React } from "react";
-import TestForm from "../components/forms/formik-test";
-
-export default { title: "Test formik form" };
-
-export const EmptyForm = () => <TestForm />;
---
+++ src/stories/forms.stories.js
@@ -0,0 +1,6 @@
+import { React } from "react";
+import SignupForm from "../components/forms/SignupForm";
+
+export default { title: "Formik forms" };
+
+export const SignupFormEmpty = () => <SignupForm />;
