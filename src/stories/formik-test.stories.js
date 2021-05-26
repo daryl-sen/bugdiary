8d3f3626067fa5a0de0ae9b9f1849b9d6ca9592d
Daryl <tsdaryl@gmail.com>
2021-05-26 12:17:14 -0700

    create formik test form

---
+++ src/stories/formik-test.stories.js
@@ -0,0 +1,6 @@
+import { React } from "react";
+import TestForm from "../components/forms/formik-test";
+
+export default { title: "Test formik form" };
+
+export const EmptyForm = () => <TestForm />;
