109ec9c45517f892c3f61e935e001c29673422d6
Daryl <tsdaryl@gmail.com>
2021-05-26 17:30:51 -0700

    remove express app ability to read cookies directly

--- helpers/authenticate-token.js
+++ helpers/authenticate-token.js
@@ -3,18 +3,12 @@ const jwt = require("jsonwebtoken");
 
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
-  let token;
-  console.log("cookies", req.cookies.jwt);
 
   if (!authHeader) {
-    if (req.cookies.jwt) {
-      token = req.cookies.jwt;
-    } else {
-      return res.status(401).json({ error: "JWT not received" });
-    }
-  } else {
-    token = authHeader.split(" ")[1];
+    return res.status(401).json({ error: "JWT not received" });
   }
+
+  const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, user) => {
     if (error) {
       return res.status(403).json({ error: "Invalid JWT" });
