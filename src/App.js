4be576b3e962622cb77cc977726fef63a6baacd1
Daryl <tsdaryl@gmail.com>
2021-05-26 20:34:36 -0700

    add redirect for LoginForm for success and failed login

--- src/App.js
+++ src/App.js
@@ -1,63 +1,25 @@
 import "./App.scss";
 import MainRouter from "./MainRouter";
-import axios from "axios";
-import { useState } from "react";
+import React, { useState } from "react";
 
 const BASE_URL = process.env.REACT_APP_API_URL;
 
+export const UserContext = React.createContext();
+
 function App() {
-  const [jwt, setJwt] = useState(null);
-
-  // const getCookie = () => {
-  //   console.log("Getting cookie");
-  //   const loginInfo = {
-  //     email: "sensworks.ca@gmail.com",
-  //     password: "password",
-  //   };
-  //   axios.post("/api/users/login", loginInfo).then((resp) => {
-  //     console.log("RESPONSE", resp.data);
-  //     setJwt(resp.data.accessToken);
-  //   });
-  // };
-
-  // const doAction = () => {
-  //   console.log("Doing something only a logged-in user can do");
-  //   const info = {
-  //     uuid: "uuid",
-  //     display_name: "New Name",
-  //   };
-  //   axios
-  //     .patch("/api/users/user", info)
-  //     .then((resp) => {
-  //       console.log("response", resp);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // };
-
-  // const logout = () => {
-  //   console.log("Logging out");
-  //   axios.post("/api/users/logout").then((resp) => {
-  //     console.log(resp.data);
-  //   });
-  // };
-
-  // const checkToken = () => {
-  //   console.log("Checking jwt");
-  //   axios
-  //     .post("/api/users/check-token")
-  //     .then((resp) => {
-  //       console.log(resp.data);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // };
+  const [userSession, setUserSession] = useState({
+    jwt: null,
+    name: null,
+    theme: "light",
+    notifications: [],
+  });
 
   return (
     <div className="App">
-      <MainRouter />
+      <UserContext.Provider value={{ ...userSession, setUserSession }}>
+        <MainRouter />
+      </UserContext.Provider>
+      JWT:{userSession.jwt}
     </div>
   );
 }
--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,14 +1,54 @@
 import StylizedForm from "./StylizedForm";
 import { useFormik } from "formik";
+import { useContext } from "react";
+import { UserContext } from "../../App";
+import axios from "axios";
+import { Redirect, useHistory } from "react-router-dom";
 
 export default function LoginForm(props) {
+  const history = useHistory();
+
+  const uinfo = useContext(UserContext);
+  // console.log(uinfo);
+
+  const loginUser = async (email, password) => {
+    const accessToken = await axios
+      .post("/api/users/login", {
+        email,
+        password,
+      })
+      .then((resp) => {
+        console.log(resp);
+        if (resp.data.accessToken) {
+          return resp.data.accessToken;
+        }
+        return null;
+      });
+
+    if (accessToken) {
+      uinfo.setUserSession((prev) => {
+        return { ...prev, jwt: "test" };
+      });
+      return true;
+    } else {
+      return false;
+    }
+  };
+
   const formik = useFormik({
     initialValues: {
       email: "",
       password: "",
     },
-    onSubmit: (values) => {
-      console.log(values);
+    onSubmit: async (values) => {
+      const { email, password } = values;
+      const authenticated = await loginUser(email, password);
+      if (authenticated) {
+        console.log("authenticated!");
+        history.push("/");
+      } else {
+        console.log("No.");
+      }
     },
   });
 
