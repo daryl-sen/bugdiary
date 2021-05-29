8ce00dba82907445a1063c15c4e52c56fbc86fdb
Daryl <tsdaryl@gmail.com>
2021-05-29 16:34:00 -0700

    install react-notification and setup basic trial

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import "./App.scss";
 import MainRouter from "./MainRouter";
 import React, { useState } from "react";
 
+import "react-notifications/lib/notifications.css";
+import {
+  NotificationContainer,
+  NotificationManager,
+} from "react-notifications";
+
 const BASE_URL = process.env.REACT_APP_API_URL;
 
 export const UserContext = React.createContext();
@@ -14,12 +20,18 @@ function App() {
     notifications: [],
   });
 
+  const testNotification = () => {
+    NotificationManager.info("Test notif");
+  };
+
   return (
     <div className="App">
       <UserContext.Provider value={{ ...userSession, setUserSession }}>
+        <NotificationContainer />
         <MainRouter />
       </UserContext.Provider>
       JWT:{userSession.jwt}
+      <button onClick={testNotification}>test notification</button>
     </div>
   );
 }
