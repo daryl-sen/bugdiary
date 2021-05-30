29967850e43e887e201df0311448970ac0ef8921
Daryl <tsdaryl@gmail.com>
2021-05-30 16:05:42 -0700

    add functionality to GET /api/diaries/ to retrieve diaries belonging to user

--- routes/diaries.js
+++ routes/diaries.js
@@ -6,9 +6,18 @@ module.exports = (models) => {
   const { Diary, User, Issue, Version, Type, Tag, Location } = models;
   router
     .get("/", authenticateToken, async (req, res) => {
-      const targetUuid = req.decodedUser.uuid;
-      console.log(req.decodedUser.id);
-      res.json(["helo"]);
+      const targetId = req.decodedUser.id;
+      try {
+        const userDiaries = await Diary.findAll({
+          where: {
+            user_id: targetId,
+          },
+        });
+        return res.json(userDiaries);
+      } catch (err) {
+        console.log(err);
+      }
+      return res.json({ error: err.errors });
     })
 
     .get("/top", (req, res) => {
