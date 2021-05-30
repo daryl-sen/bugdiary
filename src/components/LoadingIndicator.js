bd2a181291fdc774ab0852760c7d1f5c69ac3817
Daryl <tsdaryl@gmail.com>
2021-05-30 13:01:08 -0700

    fill in loading indicator with placeholder icon

--- src/components/LoadingIndicator.js
+++ src/components/LoadingIndicator.js
@@ -1,5 +1,12 @@
 import "./LoadingIndicator.scss";
+import { VscLoading } from "react-icons/vsc";
 
 export default function LoadingIndicator() {
-  return <div className="loading-indicator"></div>;
+  return (
+    <div className="loading-indicator">
+      <div>
+        <VscLoading />
+      </div>
+    </div>
+  );
 }
