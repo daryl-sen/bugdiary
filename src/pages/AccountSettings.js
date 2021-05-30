51c9b1ccad4d2ba2d3339ba33b535995b64b282a
Daryl <tsdaryl@gmail.com>
2021-05-30 13:40:48 -0700

    fix logging out did not clear react state for userSession

--- src/pages/AccountSettings.js
+++ src/pages/AccountSettings.js
@@ -3,12 +3,19 @@ import { useHistory } from "react-router";
 import SingleColumnLayout from "../components/layout/SingleColumnLayout";
 import { NotificationManager } from "react-notifications";
 
+import { useContext } from "react";
+import { UserContext } from "../App";
+
 export default function AccountSettings() {
   const history = useHistory();
+  const uinfo = useContext(UserContext);
 
   const logout = () => {
     axios.post("/api/users/logout").then((resp) => {
       NotificationManager.success("Logged out!");
+      uinfo.setUserSession((prev) => {
+        return { ...prev, jwt: null };
+      });
       history.push("/");
     });
   };
