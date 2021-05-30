68a1bb6988b77aa877079feaeb5c4f4e350fad79
Daryl <tsdaryl@gmail.com>
2021-05-30 14:59:30 -0700

    add fonts to App.scss

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,25 @@
 export default function Homepage() {
   return (
-    <main>
+    <main
+      style={{
+        padding: "2rem",
+      }}
+    >
       <h1>Welcome to Bug Diary</h1>
+      <p>
+        BugDiary is a simple-to-use issue-reporting and bug-tracking platform
+        that's built for everyone.
+      </p>
+      <h2>For You and Your Audience</h2>
+      <p>
+        If someone encounters a bug while using your project, they can report it
+        to your BugDiary without signing up for an account.
+      </p>
+      <p>
+        BugDiary is a non-technical bug tracker, meaning that anyone can pick it
+        up and start using it. You can get set up in less than 5 minutes
+        (including registration)!
+      </p>
     </main>
   );
 }
