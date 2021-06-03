8605d56f10280e1f58bbd781ea175463852b9f66
Daryl <tsdaryl@gmail.com>
2021-06-02 18:56:16 -0700

    create POST /api/diaries/diary (create) and PATCH /api/diaries/diary

--- routes/diaries.js
+++ routes/diaries.js
@@ -5,6 +5,7 @@ const { authenticateToken } = require("../helpers/authenticate-token");
 module.exports = (models) => {
   const { Diary, User, Issue, Version, Type, Tag, Location } = models;
   router
+    // all diaries belonging to the current user
     .get("/", authenticateToken, async (req, res) => {
       const targetId = req.decodedUser.id;
       try {
@@ -20,6 +21,7 @@ module.exports = (models) => {
       return res.json({ error: err.errors });
     })
 
+    // specific diary
     .get("/:uuid", async (req, res) => {
       const targetUuid = req.params.uuid;
       try {
@@ -38,6 +40,37 @@ module.exports = (models) => {
         console.log(err.errors);
       }
       return res.end("diaries route");
+    })
+
+    // create diary
+    .post("/diary", async (req, res) => {
+      try {
+        const newDiary = await Diary.create({ ...req.body });
+        return res.json(newDiary);
+      } catch (err) {
+        console.log(error);
+        return res.status(500).json(error);
+      }
+    })
+
+    // update diary
+    .patch("diary", async (req, res) => {
+      const uuid = req.body.uuid;
+      try {
+        const targetDiary = await Diary.findOne({
+          where: {
+            uuid,
+          },
+        });
+        for (const attribute in req.body) {
+          targetDiary[attribute] = req.body[attribute];
+        }
+        await targetDiary.save();
+        return res.json(targetDiary);
+      } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+      }
     });
 
   return router;
