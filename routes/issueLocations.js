e9d4be406dca2dcf8c87c20ebc30ae452621ff25
Daryl <tsdaryl@gmail.com>
2021-06-02 19:45:15 -0700

    create CREATE and READ routes for location

--- routes/issueLocations.js
+++ routes/issueLocations.js
@@ -7,38 +7,59 @@ module.exports = (models) => {
   router
 
     // create new location
-    .post("/", authenticateToken, (req, res) => {
+    .post("/", authenticateToken, async (req, res) => {
       try {
-        //
+        const newLocation = await Location.create({ ...req.body });
+        return res.json(newLocation);
       } catch (err) {
-        //
+        console.log(err);
+        return res.status(500).json(err);
       }
     })
 
     // read location
-    .get("/", authenticateToken, (req, res) => {
+    .get("/", authenticateToken, async (req, res) => {
+      const uuid = req.body.uuid;
       try {
-        //
+        const targetLocation = await Location.findOne({
+          where: {
+            uuid,
+          },
+        });
+        return res.json(targetLocation);
       } catch (err) {
-        //
+        console.log(err);
+        return res.json(err);
       }
     })
 
     // read all locations
-    .get("/", authenticateToken, (req, res) => {
+    .get("/:diaryUuid", async (req, res) => {
       try {
-        //
+        const targetDiary = await Diary.findOne({
+          where: {
+            uuid: req.params.diaryUuid,
+          },
+        });
+        console.log(await targetDiary.getLocations());
+        return res.json(await targetDiary.getLocations());
       } catch (err) {
-        //
+        console.log(err);
+        return res.json(err);
       }
     })
 
     // update location
-    .patch("/", authenticateToken, (req, res) => {
+    .patch("/", authenticateToken, async (req, res) => {
       try {
-        //
+        const targetLocation = Location.findOne({
+          where: {
+            id: req.body.id,
+          },
+        });
       } catch (err) {
-        //
+        console.log(err);
+        return res.json(err);
       }
     })
 
@@ -47,7 +68,8 @@ module.exports = (models) => {
       try {
         //
       } catch (err) {
-        //
+        console.log(err);
+        return res.json(err);
       }
     });
   return router;
