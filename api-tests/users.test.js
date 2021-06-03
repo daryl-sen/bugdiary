a5154c52b6b7e817ca87780b8153ecadb842b34f
Daryl <tsdaryl@gmail.com>
2021-06-02 20:45:22 -0700

    create test for logging in (user.test.js)

--- api-tests/users.test.js
+++ api-tests/users.test.js
@@ -3,31 +3,49 @@ const models = require("../models");
 
 const BASE_URL = "http://localhost:3000";
 
+const jwt = [];
+
 describe("/api/users/user", () => {
-  test("GET /user: creates a new user, gets user info as response", async () => {
-    //create a new user
-    const newUser = {
-      uuid: "jestuuid4",
-      display_name: "Jester",
+  // test("GET /user: creates a new user, gets user info as response", async () => {
+  //   //create a new user
+  //   const newUser = {
+  //     uuid: "jestuuid4",
+  //     display_name: "Jester",
+  //     email: "jester@fakeemail.com",
+  //     password: "password",
+  //     bio:
+  //       "User created by Jest testing. If this user still exists, something wrong has happened.",
+  //     user_type_id: 1,
+  //   };
+
+  //   const response = await axios
+  //     .post(BASE_URL + "/api/users/user", newUser)
+  //     .then((resp) => {
+  //       return resp.data;
+  //     });
+
+  //   // console.log(response);
+
+  //   expect(response.uuid).toBe(newUser.uuid);
+  //   expect(response.display_name).toBe(newUser.display_name);
+  //   expect(response.email).toBe(newUser.email);
+  //   expect(response.password).toBe(newUser.password);
+  //   expect(response.bio).toBe(newUser.bio);
+  // });
+
+  test("POST /login: users can log in and get a jwt response", async () => {
+    const loginCreds = {
       email: "jester@fakeemail.com",
       password: "password",
-      bio:
-        "User created by Jest testing. If this user still exists, something wrong has happened.",
-      user_type_id: 1,
     };
-
     const response = await axios
-      .post(BASE_URL + "/api/users/user", newUser)
+      .post(BASE_URL + "/api/users/login", loginCreds)
       .then((resp) => {
         return resp.data;
       });
 
-    // console.log(response);
+    jwt[0] = response.accessToken;
 
-    expect(response.uuid).toBe(newUser.uuid);
-    expect(response.display_name).toBe(newUser.display_name);
-    expect(response.email).toBe(newUser.email);
-    expect(response.password).toBe(newUser.password);
-    expect(response.bio).toBe(newUser.bio);
+    expect(jwt[0]).not.toBe(null);
   });
 });
