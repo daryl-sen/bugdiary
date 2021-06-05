39b90e1bd62bb33e8904abd8ec9c445f0bcfd89a
Daryl <tsdaryl@gmail.com>
2021-06-04 17:47:46 -0700

    fill in route for /api/versions

--- routes/IssueVersions.js
+++ routes/IssueVersions.js
@@ -3,51 +3,65 @@ const router = express.Router();
 const { authenticateToken } = require("../helpers/authenticate-token");
 
 module.exports = (models) => {
-  const { Issue, Diary, Version, User } = models;
+  const { Diary, Version } = models;
   router
 
     // create new version
-    .post("/", authenticateToken, (req, res) => {
+    .post("/", authenticateToken, async (req, res) => {
       try {
-        //
+        const newVersion = await Version.create({ ...req.body });
+        return res.json(newVersion);
       } catch (err) {
-        //
-      }
-    })
-
-    // read version
-    .get("/", authenticateToken, (req, res) => {
-      try {
-        //
-      } catch (err) {
-        //
+        console.log(err);
+        return res.json(err);
       }
     })
 
     // read all versions
-    .get("/", authenticateToken, (req, res) => {
+    .get("/:diaryUuid", authenticateToken, async (req, res) => {
       try {
-        //
+        const targetDiary = await Diary.findOne({
+          where: {
+            uuid: req.params.diaryUuid,
+          },
+        });
+        return res.json(await targetDiary.getVersions());
       } catch (err) {
-        //
+        console.log(err);
+        return res.json(err);
       }
     })
 
     // update version
-    .patch("/", authenticateToken, (req, res) => {
+    .patch("/", authenticateToken, async (req, res) => {
       try {
-        //
+        const targetVersion = await Version.findOne({
+          where: {
+            id: req.body.id,
+          },
+        });
+        targetVersion.name = req.body.name;
+        await targetVersion.save();
+        return res.json(targetVersion);
       } catch (err) {
-        //
+        console.log(err);
+        return res.json(err);
       }
     })
 
     // delete version
-    .delete("/", authenticateToken, (req, res) => {
+    .delete("/:id", authenticateToken, async (req, res) => {
       try {
-        //
+        const targetVersion = await Version.findOne({
+          where: {
+            id: req.params.id,
+          },
+        });
+        targetVersion.destroy();
+        return res.json({ success: true });
       } catch (err) {
-        //
+        console.log(err);
+        return res.json(err);
       }
     });
   return router;
