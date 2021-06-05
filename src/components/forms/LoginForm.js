6fcadeacbb4834e6a5c9b7c563394cff4654f8c0
Daryl <tsdaryl@gmail.com>
2021-06-05 15:51:29 -0700

    refactor and slim LoginForm component and create useUserFunctions hook

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,13 +1,5 @@
-import axios from "axios";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import LoadingIndicator from "../LoadingIndicator";
-
-// context
-import { useContext, useState } from "react";
-import { UserContext } from "../../App";
-
-// notifications
-import { NotificationManager } from "react-notifications";
 
 // form
 import StylizedForm from "./StylizedForm";
