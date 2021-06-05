b08fa5985b0802e6a04c570ab0d07c4ba7dc765d
Daryl <tsdaryl@gmail.com>
2021-06-05 15:34:38 -0700

    create blank NewDiaryForm

---
+++ src/components/forms/NewDiaryForm.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { Link, useHistory } from "react-router-dom";
+import LoadingIndicator from "../LoadingIndicator";
+
+// context
+import { useContext, useState } from "react";
+import { UserContext } from "../../App";
+
+// notifications
+import { NotificationManager } from "react-notifications";
+
+// form
+import StylizedForm from "./StylizedForm";
+import { useFormik } from "formik";
+
+export default function NewDiaryForm(props) {
+  const [loadingStatus, setLoadingStatus] = useState(false);
+  const history = useHistory();
+  const uinfo = useContext(UserContext);
+
+  return <></>;
+}
