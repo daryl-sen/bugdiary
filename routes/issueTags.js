0b7dd5f804f959dfcc0f0916901f9e5b0e9ebdb1
Daryl <tsdaryl@gmail.com>
2021-06-04 18:07:55 -0700

    fill in /api/tags routes

--- routes/issueTags.js
+++ routes/issueTags.js
@@ -7,47 +7,61 @@ module.exports = (models) => {
   router
 
     // create new tag
-    .post("/", authenticateToken, (req, res) => {
+    .post("/", authenticateToken, async (req, res) => {
       try {
-        //
+        const newTag = await Tag.create({ ...req.body });
+        return res.json(newTag);
       } catch (err) {
-        //
-      }
-    })
-
-    // read tag
-    .get("/", authenticateToken, (req, res) => {
-      try {
-        //
-      } catch (err) {
-        //
+        console.log(err);
+        return res.json(err);
       }
     })
 
     // read all tags
-    .get("/", authenticateToken, (req, res) => {
+    .get("/:diaryUuid", authenticateToken, async (req, res) => {
       try {
-        //
+        const targetDiary = await Diary.findOne({
+          where: {
+            uuid: req.params.diaryUuid,
+          },
+        });
+        return res.json(await targetDiary.getTags());
       } catch (err) {
-        //
+        console.log(err);
+        return res.json(err);
       }
     })
 
     // update tag
-    .patch("/", authenticateToken, (req, res) => {
+    .patch("/", authenticateToken, async (req, res) => {
       try {
-        //
+        const targetTag = await Tag.findOne({
+          where: {
+            id: req.body.id,
+          },
+        });
+        targetTag.name = req.body.name;
+        await targetTag.save();
+        return res.json(targetTag);
       } catch (err) {
-        //
+        console.log(err);
+        return res.json(err);
       }
     })
 
     // delete tag
-    .delete("/", authenticateToken, (req, res) => {
+    .delete("/:id", authenticateToken, async (req, res) => {
       try {
-        //
+        const targetTag = await Tag.findOne({
+          where: {
+            id: req.params.id,
+          },
+        });
+        targetTag.destroy();
+        return res.json({ success: true });
       } catch (err) {
-        //
+        console.log(err);
+        return res.json(err);
       }
     });
   return router;
