23dcd54cdb33b23c8f0e86c0b287a51f9c4af579
Daryl <tsdaryl@gmail.com>
2021-06-06 14:35:13 -0700

    create blank NewIssue component

---
+++ src/components/elements/NewIssue.js
@@ -0,0 +1,10 @@
+import "./NewIssue.scss";
+import FullScreenShade from "../elements/FullScreenShade";
+
+export default function NewIssue(props) {
+  return (
+    <>
+      <FullScreenShade />
+    </>
+  );
+}
