c8968eb8089f9c15b48f08cc82baaadc3aaedff0
Daryl <tsdaryl@gmail.com>
2021-06-05 22:08:24 -0700

    create basic mockup in DiarySetup

--- src/pages/DiarySetup.js
+++ src/pages/DiarySetup.js
@@ -1,5 +1,4 @@
 import SingleColumnLayout from "../components/layout/SingleColumnLayout";
-import NewDiaryForm from "../components/forms/NewDiaryForm";
 import WhiteBgContainer from "../components/elements/WhiteBgContainer";
 
 export default function DiarySetup() {
@@ -8,7 +7,7 @@ export default function DiarySetup() {
       styleOverride={{
         maxWidth: "500px",
         margin: "auto",
-        height: "80vh",
+        minHeight: "80vh",
         display: "flex",
         flexDirection: "column",
         justifyContent: "space-around",
@@ -16,7 +15,37 @@ export default function DiarySetup() {
     >
       <WhiteBgContainer>
         <h1>Diary Setup</h1>
-        <NewDiaryForm />
+        <p>
+          Your diary is created! Let's add some basic labels to help you
+          organize your project's issues.
+        </p>
+        <hr />
+        <h2>Version</h2>
+        <p>
+          Your project's current version. Issues can be sorted by version
+          numbers.
+        </p>
+        <input type="text" />
+        <button className="custom">Create Version</button>
+        <button className="custom button-primary">Next Step</button>
+        {/* <h2>Versions</h2>
+        <p>What's your project's current version?</p>
+        <input type="text" />
+        <button className="custom">Create Location</button>
+        <hr />
+        <h2>Locations</h2>
+        <p>Where can users expect to see issues?</p>
+        <input type="text" />
+        <button className="custom">Create Type</button>
+        <input type="checkbox" />
+        Let users create new locations.
+        <hr />
+        <h2>Types</h2>
+        <p>What types of issues can your users expect to see?</p>
+        <input type="text" />
+        <button className="custom">Create Location</button>
+        <input type="checkbox" />
+        Let users create new types. */}
       </WhiteBgContainer>
     </SingleColumnLayout>
   );
