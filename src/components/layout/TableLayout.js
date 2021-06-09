ecd6f1e38a508c9fd741ec14ffc369c4f7a6fa13
Daryl <tsdaryl@gmail.com>
2021-06-08 21:29:12 -0700

    update TableLayout to render restructured issues

--- src/components/layout/TableLayout.js
+++ src/components/layout/TableLayout.js
@@ -4,17 +4,36 @@ import NoIssuesDisplay from "../elements/NoIssuesDisplay";
 
 export default function TableLayout(props) {
   const renderIssueContainers = (issues) => {
-    if (!Array.isArray(issues) || issues.length === 0) {
-      return <NoIssuesDisplay />;
+    if (Array.isArray(issues)) {
+      return issues.map((issue) => {
+        return (
+          <IssueContainer key={issue.id} {...issue} refresh={props.refresh} />
+        );
+      });
     }
-    return issues.map((issue) => {
-      return (
-        <IssueContainer key={issue.id} {...issue} refresh={props.refresh} />
-      );
-    });
   };
 
   return (
-    <div className="table-view">{renderIssueContainers(props.issues)}</div>
+    <div className="table-view">
+      {props.issues.issuesPrioritized
+        ? renderIssueContainers(props.issues.issuesPrioritized)
+        : null}
+      {props.issues.issuesPending
+        ? renderIssueContainers(props.issues.issuesPending)
+        : null}
+      {props.issues.issuesResolved
+        ? renderIssueContainers(props.issues.issuesResolved)
+        : null}
+      {props.issues.issuesDeleted
+        ? renderIssueContainers(props.issues.issuesDeleted)
+        : null}
+
+      {props.issues.issuesPrioritized.length === 0 &&
+      props.issues.issuesPending.length === 0 &&
+      props.issues.issuesDeleted.length === 0 &&
+      props.issues.issuesResolved.length === 0 ? (
+        <NoIssuesDisplay />
+      ) : null}
+    </div>
   );
 }
