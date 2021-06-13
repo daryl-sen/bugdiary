e93e8dd15eeb595e828b0862464a92705f0e2439
Daryl <tsdaryl@gmail.com>
2021-06-12 17:21:42 -0700

    implement basic searching

--- src/components/elements/SearchPopup.js
+++ src/components/elements/SearchPopup.js
@@ -1,17 +1,53 @@
+import { useState } from "react";
 import "./SearchPopup.scss";
 import WhiteBgContainer from "./WhiteBgContainer";
 
 export default function SearchPopup(props) {
-  const modifyResults = () => {
-    props.modifyResults((prev) => {
-      return { ...prev, issuesPending: [] };
+  const [searchTerm, setSearchTerm] = useState();
+
+  const filterResults = (term, results) => {
+    if (!term || !results) {
+      return;
+    }
+    return results.filter((issue) => {
+      return (
+        issue.reference.includes(term) ||
+        issue.details.includes(term) ||
+        issue.reporter_name.includes(term) ||
+        issue.reporter_email.includes(term)
+      );
     });
+  };
+
+  const sortResults = (orderBy, results) => {};
+
+  const modifyResults = (mode, term) => {
+    if (mode === "filter") {
+      props.modifyResults((prev) => {
+        return {
+          ...prev,
+          issuesPending: filterResults(term, prev.issuesPending),
+        };
+      });
+    } else if (mode === "sort") {
+      props.modifyResults((prev) => {
+        return {
+          ...prev,
+          issuesPending: sortResults(term, prev.issuesPending),
+        };
+      });
+    }
   };
 
   return (
     <WhiteBgContainer preset="narrow">
       <h2>Search and Filter</h2>
-      <input type="text" />
+      <input
+        type="text"
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
+      />
       <button
         className="custom"
         onClick={() => {
@@ -47,7 +83,9 @@ export default function SearchPopup(props) {
           <button
             type="submit"
             className="button-primary"
-            onClick={modifyResults}
+            onClick={() => {
+              modifyResults("filter", searchTerm);
+            }}
           >
             Search
           </button>
