ab7538a44a3f442493238919bc88cd1af0155a15
Daryl <tsdaryl@gmail.com>
2021-06-15 18:49:21 -0700

    refactor authenticate-token helper function

--- helpers/authenticate-token.js
+++ helpers/authenticate-token.js
@@ -1,3 +1,12 @@
+/*
+  function will create req.auth, which contains an object
+  { 
+    status: BOOL,
+    message: STRING, // undefined is no error
+    userInfo: OBJ // undefined if there's any error
+  }
+*/
+
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
@@ -5,15 +14,15 @@ const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
 
   if (!authHeader) {
-    return res.json({ error: "JWT not received" });
+    req.auth = { status: false, message: "No JWT Received" };
   }
 
   const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, user) => {
     if (error) {
-      return res.json({ error: "Invalid JWT" });
+      req.auth = { status: false, message: "Invalid JWT" };
     }
-    req.decodedUser = user;
+    req.auth = { status: true, userInfo = user}
   });
   next();
 };
