85aed8d84df2c6a0fc4dc45abf47683959664e95
Daryl <tsdaryl@gmail.com>
2021-06-20 15:13:44 -0700

    restore imports and routes to MainRouter

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -3,6 +3,22 @@ import "./MainRouter.scss";
 import { useAppContext } from "./AppContext";
 import useUserFunctions from "./hooks/useUserFunctions";
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import TopBar from "./components/layouts/TopBar";
+import SideNav from "./components/layouts/SideNav";
+import ProtectedRoute from "./ProtectedRoute";
+// pages
+import Diary from "./components/pages/Diary";
+import LoginPage from "./components/pages/LoginPage";
+import AccountSettings from "./components/pages/AccountSettings";
+import Diaries from "./components/pages/Diaries";
+import DiarySetup from "./components/pages/DiarySetup";
+import DiaryNew from "./components/pages/DiaryNew";
+import Homepage from "./components/pages/Homepage";
+import AboutUs from "./components/pages/AboutUs";
+import Signup from "./components/pages/Signup";
+
 export default function MainRouter(props) {
   const [menuState, setMenuToggle] = useState(false);
   const { context, setContext } = useAppContext();
@@ -13,16 +29,42 @@ export default function MainRouter(props) {
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <>
-      <button
-        onClick={() => {
-          setContext((prev) => {
-            return { ...prev, name: "hello" };
-          });
-        }}
-      >
-        Click
-      </button>
-    </>
+    <Router>
+      <TopBar toggleMenu={toggleMenu} menuState={menuState} />
+      <SideNav menuState={menuState} toggleMenu={toggleMenu} />
+      <Switch>
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+        <Route exact path="/account">
+          <ProtectedRoute target="account">
+            <AccountSettings />
+          </ProtectedRoute>
+        </Route>
+        <Route exact path="/diaries">
+          <ProtectedRoute target="diaries">
+            <Diaries />
+          </ProtectedRoute>
+        </Route>
+        <Route exact path="/diary/:uuid/:functionView?">
+          <Diary />
+        </Route>
+        <Route exact path="/login">
+          <LoginPage />
+        </Route>
+        <Route exact path="/new">
+          <DiaryNew />
+        </Route>
+        <Route exact path="/setup/:uuid">
+          <DiarySetup />
+        </Route>
+        <Route exact path="/about">
+          <AboutUs />
+        </Route>
+        <Route exact path="/signup">
+          <Signup />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
