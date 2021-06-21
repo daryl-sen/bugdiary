55aa30bd07b79b8595e659411851dd4bf9545158
Daryl <tsdaryl@gmail.com>
2021-06-21 10:50:12 -0700

    create DiaryContext component

---
+++ src/DiaryContext.js
@@ -0,0 +1,16 @@
+import React, { useContext, useState } from "react";
+export const DiaryContext = React.createContext();
+
+export function useDiaryContext() {
+  return useContext(DiaryContext);
+}
+
+export default function DiaryContextProvider(props) {
+  const [diaryContext, setDiaryContext] = useState({});
+
+  return (
+    <AppContext.Provider value={{ diaryContext, setDiaryContext }}>
+      {props.children}
+    </AppContext.Provider>
+  );
+}
