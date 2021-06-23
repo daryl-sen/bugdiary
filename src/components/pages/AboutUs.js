d036dcb03d691b5613130cf101e92f69316e9a36
Daryl <tsdaryl@gmail.com>
2021-06-23 16:52:51 -0700

    update about us page

--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -1,3 +1,44 @@
+import SingleColumnLayout from "../layouts/SingleColumnLayout";
+
 export default function AboutUs() {
-  return <div>Hello</div>;
+  return (
+    <SingleColumnLayout preset="narrow-centered">
+      <div>
+        <h1>About BugDiary.com</h1>
+        <p>
+          Thanks for checking us out! We'll have some really useful information
+          here soon. In the meantime, please note that BugDiary is current in
+          the <b>testing phase</b>.
+        </p>
+        <p>
+          While most of the core features are ready, we're still working to
+          improve stability, performance, and error-handling. Account management
+          features are currently unavailable.
+        </p>
+
+        <h2>Using Development Software</h2>
+        <p>
+          Any data you created on BugDiary may be deleted during or at the end
+          of this testing phase. We'll try our best to preserve any diaries and
+          issues you created during this time, but this is not guaranteed!
+        </p>
+        <h2>A Word of Thanks</h2>
+        <p>
+          Thanks once again for visiting bugdiary.com. We sincerely hope you'll
+          give BugDiary a try when it's fully ready for deployment.
+        </p>
+        <p
+          style={{
+            marginTop: "3rem",
+          }}
+        >
+          Daryl Tang
+          <br />
+          Creator and Software Engineer
+          <br />
+          BugDiary.com
+        </p>
+      </div>
+    </SingleColumnLayout>
+  );
 }
