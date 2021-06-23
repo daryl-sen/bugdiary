9ff47f9d8e2dad73e78dc3f29a0a31963efc7938
Daryl <tsdaryl@gmail.com>
2021-06-23 13:56:09 -0700

    create basic splash screen on homepage

--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -1,25 +1,36 @@
+import "./Homepage.scss";
+import { Link } from "react-router-dom";
+
 export default function Homepage() {
   return (
-    <main
-      style={{
-        padding: "2rem",
-      }}
-    >
-      <h1>Welcome to Bug Diary</h1>
-      <p>
-        BugDiary is a simple-to-use issue-reporting and bug-tracking platform
-        that's built for everyone.
-      </p>
-      <h2>For You and Your Audience</h2>
-      <p>
-        If someone encounters a bug while using your project, they can report it
-        to your BugDiary without signing up for an account.
-      </p>
-      <p>
-        BugDiary is a non-technical bug tracker, meaning that anyone can pick it
-        up and start using it. You can get set up in less than 5 minutes
-        (including registration)!
-      </p>
-    </main>
+    <>
+      <section id="splash">
+        <div id="welcome">
+          <h1>Welcome to Bug Diary</h1>
+          <p>
+            Bug Diary is an online issue-reporting and bug-tracking platform. No
+            payments, no sign-ups required, and you can get your Bug Diary up
+            and running in <Link to="/new">less than 30 seconds</Link>!
+          </p>
+          <div style={{ maxWidth: "50%", margin: "auto" }}>
+            <Link to="/new">
+              <button className="custom button-primary">
+                Get Setup In 30 seconds
+              </button>
+            </Link>
+            <Link to="/signup">
+              <button className="custom button-primary">
+                Sign Up For More Features
+              </button>
+            </Link>
+          </div>
+          <p></p>
+        </div>
+      </section>
+
+      <section>
+        <h2>Built for you and your team</h2>
+      </section>
+    </>
   );
 }
