a5bdae22cdbf938419eb77b3ca5d7a98550d697f
Daryl <tsdaryl@gmail.com>
2021-06-23 15:48:31 -0700

    refactor logo button to redirect to homepage if not logged in, diaries if logged in

--- src/components/layouts/TopBar.js
+++ src/components/layouts/TopBar.js
@@ -1,8 +1,12 @@
 import "./TopBar.scss";
 import { CgMenu, CgClose } from "react-icons/cg";
-import { Link } from "react-router-dom";
+import { useAppContext } from "../../AppContext";
+import { useHistory } from "react-router";
 
 export default function TopBar(props) {
+  const history = useHistory();
+  const { context } = useAppContext();
+
   return (
     <header id="top-bar">
       <button id="side-nav-toggle">
@@ -12,9 +16,14 @@ export default function TopBar(props) {
           <CgMenu onClick={props.toggleMenu} />
         )}
       </button>
-      <div id="title">
-        <Link to="/diaries">BugDiary.com</Link>
-      </div>
+      <button
+        id="title"
+        onClick={() => {
+          return history.push(context.jwt ? "/diaries" : "/");
+        }}
+      >
+        BugDiary.com
+      </button>
     </header>
   );
 }
