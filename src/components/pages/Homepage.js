fe63530404816253c263cd26c24dd10b12b4bd27
Daryl <tsdaryl@gmail.com>
2021-06-23 15:39:51 -0700

    update homepage text

--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -11,7 +11,8 @@ export default function Homepage() {
           <p>
             Bug Diary is an online issue-reporting and bug-tracking platform. No
             payments, no sign-ups required, and you can get your Bug Diary up
-            and running in <Link to="/new">less than 30 seconds</Link>!
+            and running in <Link to="/new">as little as 30 seconds</Link>
+            <sup>1</sup>! What's there to lose?
           </p>
           <div style={{ maxWidth: "50%", margin: "auto" }}>
             <Link to="/new">
@@ -33,14 +34,18 @@ export default function Homepage() {
         imgURL="https://res.cloudinary.com/sensworks/image/upload/v1624484767/bugdiary/twogirlsdebugging_qpeyrr.jpg"
         textPosition="left"
       >
-        <h2>Built for You and Your Team</h2>
+        <h2>General-Purpose, No Learning Curve</h2>
         <p>
-          Create a Bug Diary is as little as 30 seconds, then share its unique
-          URL with your teammates.
+          Bug Diary is a non-technical issue-tracker, meaning that anybody can
+          pick it up and start using it right away!
         </p>
         <p>
-          BugDiary is a non-technical bug tracker, meaning that anyone can pick
-          it up and start using it. You can get set up in less than 30 seconds!
+          We follow the KISS principle of software design -{" "}
+          <b>keep it stupid simple</b>. Thanks to this, Bug Diary can be used
+          for projects of various types.
+        </p>
+        <p>
+          <Link>Is BugDiary Right For My Project?</Link>
         </p>
       </HighlightedSection>
 
@@ -48,15 +53,19 @@ export default function Homepage() {
         imgURL="https://res.cloudinary.com/sensworks/image/upload/v1624484767/bugdiary/debuglaptop_d4kgwn.jpg"
         textPosition="right"
       >
-        <h2>Built for You and Your Team</h2>
+        <h2>Built For You And Your Team</h2>
         <p>
-          Reports are generated each time you login, telling you how many issues
-          have been reported or solved across all your BugDiaries since your
-          last login.
+          It's extremely easy to collaborate using Bug Diary. Neither you nor
+          your teammates need to sign up for an account.
         </p>
         <p>
-          Collaborate with other BugDiary users to track multiple projects
-          together.
+          Just create a bug diary, send its unique URL to your team, and start
+          tracking issues. It's that easy!
+        </p>
+        <p>
+          <Link to="/new">
+            <button className="custom button-primary">Get Started</button>
+          </Link>
         </p>
       </HighlightedSection>
     </>
