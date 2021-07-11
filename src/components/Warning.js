2dc6f882cf37a4e3733b0fbef4c84adf55db5160
Daryl <tsdaryl@gmail.com>
2021-07-10 17:17:46 -0700

    update warning message

--- src/components/Warning.js
+++ src/components/Warning.js
@@ -4,8 +4,8 @@ export default function Warning(props) {
   return (
     <div className="persistent-warning">
       This software is currently in the beta testing stage. Some features may be
-      unavailable or work in unexpected ways.
-      <button onClick={props.exit}>Dismiss</button>
+      unavailable or work in unexpected ways.{" "}
+      <span onClick={props.exit}>(Dismiss)</span>
     </div>
   );
 }
