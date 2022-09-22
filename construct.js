d3c41ac7476fed3880d335258ce6c443b9961eaa
Daryl <tsdaryl@gmail.com>
2022-09-22 15:05:31 -0700

    change default db to sqlite

--- construct.js
+++ construct.js
@@ -5,36 +5,56 @@ This file constructs the config/config.json file using environment variables.
 require("dotenv").config();
 const fs = require("fs");
 
+// UPDATE 22/09/2022: Project has been deprecated, use SQLite database for demo
+
+// const newData = {
+//   development: {
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     dialect: process.env.DB_DIALECT,
+//     define: {
+//       underscored: true,
+//     },
+//   },
+//   test: {
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     dialect: process.env.DB_DIALECT,
+//     define: {
+//       underscored: true,
+//     },
+//   },
+//   production: {
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     dialect: process.env.DB_DIALECT,
+//     define: {
+//       underscored: true,
+//     },
+//   },
+// };
+
 const newData = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    define: {
-      underscored: true,
-    },
+    database: "database_development",
+    dialect: "sqlite",
+    storage: "./database.sqlite",
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    define: {
-      underscored: true,
-    },
+    database: "database_development",
+    dialect: "sqlite",
+    storage: "./database.sqlite",
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    define: {
-      underscored: true,
-    },
+    database: "database_development",
+    dialect: "sqlite",
+    storage: "./database.sqlite",
   },
 };
 
